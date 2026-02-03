c0e9ee7e42329eeaa375ca117ddac6cfaedc4bfb
sakethraja11 <sakethraja11@gmail.com>
2026-02-03 20:18:02 +0530

    Add files via upload

---
+++ Selenium/Project/Activity4.java
@@ -0,0 +1,60 @@
+package Project;
+
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class Activity4 {
+	
+	WebDriver driver ;
+	WebDriverWait wait;
+	
+	 @BeforeClass
+	    public void initiate() {
+		 //wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	        driver = new ChromeDriver();
+	        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	        driver.manage().window().maximize();
+	        driver.get("https://alchemy.hguy.co/lms");
+	        Reporter.log("Opening Chrome Browser", true);
+	    }
+	 
+	
+	 @Test
+	 public void verifySecondPopularCourse() {
+		 WebElement secondCourse = driver.findElement(By.xpath("(//h3[contains(@class,'entry-title')])[2]"));
+		 JavascriptExecutor js = (JavascriptExecutor)driver;
+		 js.executeScript("arguments[0].scrollIntoView(true)",secondCourse );
+		
+				 
+		 Assert.assertEquals(secondCourse.getText(), "Email Marketing Strategies");
+	 }
+	 
+	
+	
+	 @AfterClass
+	 public void tearDowun() {
+		 driver.quit();
+		 Reporter.log("Closing Chrome");
+	 }
+	 
+	 
+
+
+}
